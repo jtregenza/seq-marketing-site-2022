adb9be781c02ae95c21b04566f7ee44ff36aa04d
Josh Tregenza <josh@joshtregenza.com>
2024-05-30 15:10:37 +1000

    minify

--- js/themeSwitch.js
+++ js/themeSwitch.js
@@ -1,17 +1,3 @@
-const toggle = document.getElementById("toggle");
-
-const theme = window.localStorage.getItem("theme");
-
-/* checks if the theme stored in localStorage is dark
-if yes apply the dark theme to the body */
-if (theme === "light") document.body.classList.add("light");
-
-// event listener stops when the change theme button is clicked
-toggle.addEventListener("click", () => {
-   document.body.classList.toggle("light");
-   if (theme === "light") {
-     window.localStorage.setItem("theme", "dark");
-   } else window.localStorage.setItem("theme", "light");
-});
+const toggle=document.getElementById("toggle"),theme=window.localStorage.getItem("theme");"light"===theme&&document.body.classList.add("light"),toggle.addEventListener("click",()=>{document.body.classList.toggle("light"),"light"===theme?window.localStorage.setItem("theme","dark"):window.localStorage.setItem("theme","light")});
 
 
